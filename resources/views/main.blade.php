7043a151f5da87687f0c2f18185431a1ea2a366c
Daniil Vassiljev <mtalhead313@gmail.com>
2023-12-03 21:33:22 +0200

    Initial commit

---
+++ resources/views/main.blade.php
@@ -0,0 +1,14 @@
+@extends ('layouts.app')
+
+@section
+<div class="x_title">
+    <h3>Test</h3>
+    <div class="clearfix"></div>
+</div>
+<div>
+    <div class="demo-container" style="height: 250px;">
+    <h4>Welcome you son of a bitch</h4>
+    </div>
+
+</div>
+@endsection
