c939b68e68246dfba508fe506db7ea3f4478d073
Daniil Vassiljev <mtalhead313@gmail.com>
2024-04-09 21:29:33 +0300

    Done with author info block

--- resources/views/artist_views/artistInfo.blade.php
+++ resources/views/artist_views/artistInfo.blade.php
@@ -16,50 +16,74 @@
     }   
     @endphp
 
-    <div style="width: 100%; height: 200px;">
+    <div style="width: 100%; height: 180px;">
         <img src="{{url ($artist -> banner_url)}}" alt="" style="width: 100%; height: 300px; object-fit:cover; object-position: 50% 50%;">
     </div>
     <div class="container">
-    <div class="w-25">
+    <div class="w-25 d-flex flex-column justify-content-center">
         <div>
-            <div class="border rounded border-light artist-img">
-                <img src="{{url ($artist -> picture_url)}}" alt="" class="w-100 ">
+            <div class="artist-img w-100 d-flex justify-content-center align-items-center">
+                <img src="{{url ($artist -> picture_url)}}" alt="" class="Info-Image">
             </div>
         </div>
         <div>
-            <h1 style="font-family: 'Audiowide', sans-serif; color: white">{{$artist -> name}}</h1>
+            <h1 class="info-header">{{$artist -> name}}</h1>
         </div>
-        <div class="d-flex flex-row">
+        <div class="d-flex flex-row justify-content-center align-content-center">
             @if ($like == 0) 
             <div class="d-flex flex-row">
                 <a href="/like_artist/{{$artist -> id}}"><img src="{{asset('images/like.png')}}" alt="" style="width: 22px; height: 22px; margin-right: 6px;"></a>
             </div>
             <div class="white-text">
-                <span style="color: white;">| {{$artist_likes}} </span>
+                <span style="color: white; font-size:20px;">| {{$artist_likes}}</span>
             </div>      
             @elseif (Auth::user() == null)
             <div>
                 <button class="login_button" data-toggle="modal" data-target="#loginModal"><img src="{{asset('images/like.png')}}" alt="" style="width: 22px; height: 22px; margin-right: 6px;"></button>
             </div>
             <div>
-                <span>| {{$artist_likes}} </span>
+                <span style="color: white; font-size:20px;"> {{$artist_likes}}</span>
             </div>    
             @else ($like == 1) 
             <div>
                 <a href="/unlike_artist/{{$artist -> id}}"><img src="{{asset('images/liked.png')}}" alt="" style="width: 22px; height: 22px; margin-right: 6px;"></a>
             </div>
             <div>
-                <span>| {{$artist_likes}} </span>
+                <span style="color: white; font-size:20px;"> {{$artist_likes}} </span>
             </div>    
             @endif
         </div>
-        <div>
+
+        <div class="w-100 d-flex flex-row justify-content-center">
+            <div class="w-25 h-25 d-flex justify-content-center">           
+                <a href="{{$artist-> spotify_link}}">
+                    <img src="{{asset('images/links_images/spotify.png')}}" alt="" style="width: 36px; height:36px;">
+                </a>
+            </div>
+
+            <div class="w-25 h-25 d-flex justify-content-center">           
+                <a href="{{$artist-> apple_music_link}}">
+                    <img src="{{asset('images/links_images/apple.png')}}" alt="" style="width: 36px; height:36px;">
+                </a>
+            </div>
+
+            <div class="w-25 h-25 d-flex justify-content-center">           
+                <a href="{{$artist-> youtube_link}}">
+                    <img src="{{asset('images/links_images/youtube.png')}}" alt="" style="width: 36px; height:36px;">
+                </a>
+            </div>
+        </div>
+
+        <div class="background-block">
             <div>
-                <h2>About {{$artist -> name}}:</h2>
+                <h2 class="desc-header">About {{$artist -> name}}:</h2>
             </div>
             <div>
-                <p>{{$artist -> description}}</p>
+                <p class="desc-text" id="desc-text">{{$artist -> description}}</p>
             </div>
+            <button class="read-more-button m-0 p-0" data-target="#desc-text" >
+                Read more...
+            </button>
         </div>
         <div>
             <div>
@@ -75,10 +99,34 @@
                 <p>{{$user -> username}}</p>
             </div>
             <div>{{$comment -> content}}</div>
+            <hr>
             @endforeach
+            <form action="">
+                <input type="text" placeholder="Add comment">
+            </form>
         </div>
     </div>
 </div>
+
+<script>
+const readMoreButtons = document.querySelectorAll('.read-more-button');
+    readMoreButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const target = document.querySelector(button.dataset.target);
+            target.classList.toggle('expanded');
+            button.classList.toggle('expanded');
+
+            if (button.classList.contains('expanded')) {
+            // Clear the description text
+            button.textContent = 'Read less...';
+            } else {
+            // Restore the description text
+            button.textContent = 'Read more...';
     
-    @endsection
+        }
+    });
+});
+</script>
+    
+@endsection
     
