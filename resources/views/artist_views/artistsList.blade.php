4547dfd811d179a5ab1149c9c1881142e871fb2f
Daniil Vassiljev <mtalhead313@gmail.com>
2024-04-30 11:05:41 +0300

    Made mobile for artist views

--- resources/views/artist_views/artistsList.blade.php
+++ resources/views/artist_views/artistsList.blade.php
@@ -11,40 +11,41 @@
                 $username = "";
             }
         @endphp
-    <div class="Header-List">
-        <p>ARTISTS</p>
-    </div>
+        <div class="Header-List">
+            <p>ARTISTS</p>
+        </div>
 
-    <div class="d-flex justify-content-evenly">
-        @foreach ($artists as $artist)
-        @php
-            $crypt_artist = Crypt::encrypt($artist->id);
-        @endphp
-                <a href="/artist/{{$crypt_artist}}" class="card">
-                    <div class="mt-2 mb-3 d-flex justify-content-center">
-                        <img src="{{url ($artist -> picture_url)}}" alt="" style="width: 185px; height: 185px; border-radius: 5px; object-fit: cover;">
-                    </div>
-                    <div style="margin-left: 10px; max-width: 100%;">
-                        <p class="card-text-bigger-artist">{{$artist -> name}}</p>
-                    </div>
-                </a>
-            @if ($loop->iteration % 6 == 0 && $loop->iteration!= $loop->count)
+        <div class="row">
+            @foreach ($artists as $artist)
+            @php
+                $crypt_artist = Crypt::encrypt($artist->id);
+            @endphp
+                <div class="col-12 col-sm-6 col-md-4 col-lg-2 mb-4">
+                    <a href="/artist/{{$crypt_artist}}" class="card">
+                        <div class="mt-2 mb-3 d-flex justify-content-center align-items-center" style="width: 100%; padding-top: 90%; position: relative;">
+                            <img src="{{url ($artist -> picture_url)}}" alt="" class="mt-1 image_artist_list">
+                        </div>
+                        <div style="margin-left: 10px; max-width: 100%;">
+                            <p class="card-text-bigger-artist">{{$artist -> name}}</p>
+                        </div>
+                    </a>
                 </div>
-                <div class="d-flex justify-content-evenly">
-            @endif
-        @endforeach
+                @if ($loop->iteration % 6 == 0 && $loop->iteration!= $loop->count)
+                    </div>
+                    <div class="row">
+                @endif
+            @endforeach
+        </div>
+        <div class="d-flex w-100 justify-content-center align-content-center mt-3">
+            {!! $artists->links() !!}
+        </div>
     </div>
-    <div class="d-flex w-100 justify-content-center align-content-center mt-3">
-        {!! $artists->links() !!}
-    </div>
-</div>
 
 <script>
 const currentUrl = window.location.href;
 
 let crypt = "{{$crypt_user}};"
 const username ="{{$username}}";
-// Check if the URL contains a specific string (e.g. "user-albums")
 if (currentUrl.includes("liked-artists")) {
     if (crypt != 0) {
         document.querySelector(".Header-List p").innerHTML = `Artists Liked by <a href="/user/${crypt}" class="Header-List-1">${username}</a>`;
@@ -52,6 +53,4 @@ if (currentUrl.includes("liked-artists")) {
     }
 }
 </script>
-    
-    
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,115 +53,16 @@
         </form>
         @if(Auth::guest())
         <div class="login_buttons">
-        <div class="mb-2">
-          <button class="login_button" data-toggle="modal" data-target="#loginModal">
-          LOG IN
-        </button>
-      </div>
-    
-      <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered" role="document">
-              <div class="modal-content border-b">
-                  <div class="login">
-                      <div class="cross" data-dismiss="modal" aria-label="Close">
-                          <button type="button" class="cross-button "> <span aria-hidden="true">&times;</span></button>
-                      </div>
-                      <div class="w-100">
-                          <div>
-                              <h1 class="login-header">LOG IN</h1>
-                          </div>
-                          
-                          @if (session()->has('error_login'))
-                              <div class="alert error-login">
-                                  {{session()->get('error_login')}}
-                              </div>
-                          @endif
-                          <div class="d-flex w-100 flex-column justify-content-center align-items-center">
-                              <form action="{{url('/login-auth')}}" method="POST" class="form">
-                                  @csrf
-                                  <div class="w-100 d-flex justify-content-center align-center mt-2">
-                                      <input type="email" class="login-input" name="email" placeholder="Email" required autofocus>
-                                  </div>
-                                  <div class="w-100 d-flex justify-content-center align-center mt-3 mb-4">
-                                      <input type="password" class="login-input" name="password" placeholder="Password" minlength="6" required>
-                                  </div>
-                                  <div class="w-100 d-flex remember mb-3">
-                                    <input type="checkbox" id="remember" name="remember" class="ms-5 form-check-input">
-                                    <label for="remember" class="remember-me">
-                                      Remember me
-                                    </label>
-                                  </div>
-                                  <div class="w-100 d-flex justify-content-center mt-2 mb-4">
-                                      <button type="submit" class="login-button" name="login">Log in</button>
-                                  </div>
-                              </form>
-                              <hr style="border: 1px solid white; width:80%;">
-                              <div class="mb-3">
-                                  <p class="login-undertext">You don't have an account? <a data-toggle="modal" data-target="#signupModal" data-dismiss="modal" aria-label="Close" class="login-undertext-button">Create account here.</a></p>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-        <div class="mb-2">
-          <button class="login_button" data-toggle="modal" data-target="#signupModal">
-            SIGN UP
+          <div class="mb-2">
+            <button class="login_button" data-toggle="modal" data-target="#loginModal">
+            LOG IN
           </button>
-        </div>
-
-        <div class="modal fade" id="signupModal" tabindex="-1" role="dialog" aria-labelledby="signupModalLabel" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered" role="document">
-              <div class="modal-content border-b">
-                  <div class="login">
-                    <div class="cross" data-dismiss="modal" aria-label="Close">
-                      <button type="button" class="cross-button "> <span aria-hidden="true">&times;</span></button>
-                    </div>
-                      <div class="w-100">
-                          <div>
-                              <p class="login-header">SIGN UP</p>
-                          </div>
-                          @if (session()->has('error_signup'))
-                          <div class="alert error-login-1">
-                              {{session()->get('error_signup')}}
-                          </div>
-                          @endif
-                          <div class="d-flex w-100 flex-column justify-content-center align-items-center h-75 ">
-                              <form action="{{url('/register')}}" method="POST" class="form" enctype="multipart/form-data">
-                                  @csrf
-                                  <div class="w-100 d-flex justify-content-center align-items-center flex-column align-center mt-3">
-                                    <img id="imagePreview" src="#" alt="Image preview" style="display: none; width: 45%; height: 45%; border:3#808080px solid #808080; border-radius: 5px;" class="mb-3 mt-0"/>
-                                    <input type="file" id="imageInput" name="avatar_url" class="img-input">
-                                    <label for="imageInput"  id="imageInputLabel" class="edit-button m-0">Insert Avatar</label>
-                                  </div>
-                                  <div class="w-100 d-flex justify-content-center align-center mt-3">
-                                      <input type="text" class="login-input" name="username" placeholder="Username" required autofocus>
-                                  </div>
-                                  <div class="w-100 d-flex justify-content-center align-center mt-3">
-                                      <input type="email" class="login-input" name="email" placeholder="Email" required>
-                                  </div>
-                                  <div class="w-100 d-flex justify-content-center align-center mt-3">
-                                      <input type="password" class="login-input" name="password" placeholder="Password" minlength="6" required>
-                                  </div>
-                                  <div class="w-100 d-flex justify-content-center align-center mt-3">
-                                      <input type="password" class="login-input" name="password_confirmation" placeholder="Confirm Password" minlength="6" required>
-                                  </div>
-                                  <div class="w-100 d-flex justify-content-center mt-3 mb-3">
-                                      <button type="submit" class="login-button" name="login">Sign up</button>
-                                  </div>
-                              </form>
-                              <hr style="border: 1px solid white; width:80%; margin-top: 1px;">
-                              <div class="mb-3">
-                                  <p class="login-undertext">You already have an account? <a data-toggle="modal" data-target="#loginModal" data-dismiss="modal" aria-label="Close" class="login-undertext-button">Log in here.</a></p>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
           </div>
-      </div>
-
+          <div class="mb-2">
+            <button class="login_button" data-toggle="modal" data-target="#signupModal">
+              SIGN UP
+            </button>
+          </div>
       </div>
         @else
         <li class="avatar-button">
@@ -180,6 +81,104 @@
       </div>
     </div>
   </nav>
+
+  <div class="modal fade" id="signupModal" tabindex="-1" role="dialog" aria-labelledby="signupModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content border-b">
+            <div class="login">
+              <div class="cross" data-dismiss="modal" aria-label="Close">
+                <button type="button" class="cross-button "> <span aria-hidden="true">&times;</span></button>
+              </div>
+                <div class="w-100">
+                    <div>
+                        <p class="login-header">SIGN UP</p>
+                    </div>
+                    @if (session()->has('error_signup'))
+                    <div class="alert error-login-1">
+                        {{session()->get('error_signup')}}
+                    </div>
+                    @endif
+                    <div class="d-flex w-100 flex-column justify-content-center align-items-center h-75 ">
+                        <form action="{{url('/register')}}" method="POST" class="form" enctype="multipart/form-data">
+                            @csrf
+                            <div class="w-100 d-flex justify-content-center align-items-center flex-column align-center mt-3">
+                              <img id="imagePreview" src="#" alt="Image preview" style="display: none; width: 45%; height: 45%; border:3#808080px solid #808080; border-radius: 5px;" class="mb-3 mt-0"/>
+                              <input type="file" id="imageInput" name="avatar_url" class="img-input">
+                              <label for="imageInput"  id="imageInputLabel" class="edit-button m-0">Insert Avatar</label>
+                            </div>
+                            <div class="w-100 d-flex justify-content-center align-center mt-3">
+                                <input type="text" class="login-input" name="username" placeholder="Username" required autofocus>
+                            </div>
+                            <div class="w-100 d-flex justify-content-center align-center mt-3">
+                                <input type="email" class="login-input" name="email" placeholder="Email" required>
+                            </div>
+                            <div class="w-100 d-flex justify-content-center align-center mt-3">
+                                <input type="password" class="login-input" name="password" placeholder="Password" minlength="6" required>
+                            </div>
+                            <div class="w-100 d-flex justify-content-center align-center mt-3">
+                                <input type="password" class="login-input" name="password_confirmation" placeholder="Confirm Password" minlength="6" required>
+                            </div>
+                            <div class="w-100 d-flex justify-content-center mt-3 mb-3">
+                                <button type="submit" class="login-button" name="login">Sign up</button>
+                            </div>
+                        </form>
+                        <hr style="border: 1px solid white; width:80%; margin-top: 1px;">
+                        <div class="mb-3">
+                            <p class="login-undertext">You already have an account? <a data-toggle="modal" data-target="#loginModal" data-dismiss="modal" aria-label="Close" class="login-undertext-button">Log in here.</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+  <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content border-b">
+            <div class="login">
+                <div class="cross" data-dismiss="modal" aria-label="Close">
+                    <button type="button" class="cross-button "> <span aria-hidden="true">&times;</span></button>
+                </div>
+                <div class="w-100">
+                    <div>
+                        <h1 class="login-header">LOG IN</h1>
+                    </div>
+                    
+                    @if (session()->has('error_login'))
+                        <div class="alert error-login">
+                            {{session()->get('error_login')}}
+                        </div>
+                    @endif
+                    <div class="d-flex w-100 flex-column justify-content-center align-items-center">
+                        <form action="{{url('/login-auth')}}" method="POST" class="form">
+                            @csrf
+                            <div class="w-100 d-flex justify-content-center align-center mt-2">
+                                <input type="email" class="login-input" name="email" placeholder="Email" required autofocus>
+                            </div>
+                            <div class="w-100 d-flex justify-content-center align-center mt-3 mb-4">
+                                <input type="password" class="login-input" name="password" placeholder="Password" minlength="6" required>
+                            </div>
+                            <div class="w-100 d-flex remember mb-3">
+                              <input type="checkbox" id="remember" name="remember" class="ms-5 form-check-input">
+                              <label for="remember" class="remember-me">
+                                Remember me
+                              </label>
+                            </div>
+                            <div class="w-100 d-flex justify-content-center mt-2 mb-4">
+                                <button type="submit" class="login-button" name="login">Log in</button>
+                            </div>
+                        </form>
+                        <hr style="border: 1px solid white; width:80%;">
+                        <div class="mb-3">
+                            <p class="login-undertext">You don't have an account? <a data-toggle="modal" data-target="#signupModal" data-dismiss="modal" aria-label="Close" class="login-undertext-button">Create account here.</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
   <div>
     @yield('content')
   </div>
